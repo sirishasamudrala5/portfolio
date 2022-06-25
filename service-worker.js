890e582fcb182bdc75773cc2919030a2568faa1c
sirisha pv <sirishapv@sirishas-MacBook-Pro.local>
2022-06-25 23:13:28 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f66ebce0142a326c55d82a7fbb0552a0.js"
+  "/portfolio/precache-manifest.c02c5fc82934149f48b90be4691456ae.js"
 );
 
 workbox.clientsClaim();
