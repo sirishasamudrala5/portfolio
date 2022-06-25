0773f57173ddc6542178246abd29a8860effc923
sirisha pv <sirishapv@sirishas-MacBook-Pro.local>
2022-06-25 22:51:30 +0530

    timeline map - bug fix

--- src/components/common/TimeLine.jsx
+++ src/components/common/TimeLine.jsx
@@ -4,7 +4,7 @@ const TimeLine = (props) => {
   const renderItems = () => {
     const children = []
     if(props.ProfJourney){
-      props.ProfJourney.map((item, key) => {
+      props.ProfJourney.map((item, key) => (
       children.push(<div className="card">
       <div className="info">
         <h2 className="title">{item.title}</h2>
@@ -13,13 +13,14 @@ const TimeLine = (props) => {
         <p>{item.content}</p>
       </div>
     </div>)
-      })
+      ))
     }
     return(<div className="outer">
       {children}
       </div>)
   }
-    return (<div className="timeline">
+
+  return (<div className="timeline">
     {renderItems()}
   </div>)
 }
