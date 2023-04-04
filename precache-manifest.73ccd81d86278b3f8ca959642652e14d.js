481dfafe356910e82632ec40eaea3c201077911b
sirishasamudrala5 <sirisha.samudrala5@gmail.com>
2023-04-05 00:53:35 +0530

    Updates

---
+++ precache-manifest.73ccd81d86278b3f8ca959642652e14d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "c52d2b3a1cadc89ff6b3",
+    "url": "/portfolio/static/css/main.ca861577.chunk.css"
+  },
+  {
+    "revision": "c52d2b3a1cadc89ff6b3",
+    "url": "/portfolio/static/js/main.c52d2b3a.chunk.js"
+  },
+  {
+    "revision": "75aa2d861595ad62f4c5",
+    "url": "/portfolio/static/js/1.75aa2d86.chunk.js"
+  },
+  {
+    "revision": "eca41f17f9fac7b8c76d",
+    "url": "/portfolio/static/css/2.4acb6833.chunk.css"
+  },
+  {
+    "revision": "eca41f17f9fac7b8c76d",
+    "url": "/portfolio/static/js/2.eca41f17.chunk.js"
+  },
+  {
+    "revision": "ddd875d396fb8464d0bb",
+    "url": "/portfolio/static/js/runtime~main.ddd875d3.js"
+  },
+  {
+    "revision": "3a1d57bbd2f6205550a0e53475648d43",
+    "url": "/portfolio/static/media/Resume.3a1d57bb.pdf"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/portfolio/static/media/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/portfolio/static/media/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/portfolio/static/media/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/portfolio/static/media/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/portfolio/static/media/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/portfolio/static/media/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/portfolio/static/media/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/portfolio/static/media/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/portfolio/static/media/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/portfolio/static/media/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "7b18e1bf54898a45b78a1a36806a4bc8",
+    "url": "/portfolio/static/media/fa-regular-400.7b18e1bf.svg"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/portfolio/static/media/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/portfolio/static/media/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "0c12ff655da379ab518f8afe0b3ffe15",
+    "url": "/portfolio/static/media/fa-brands-400.0c12ff65.svg"
+  },
+  {
+    "revision": "b45a634efe8a9e0a3938a86c5e216417",
+    "url": "/portfolio/static/media/fa-solid-900.b45a634e.svg"
+  },
+  {
+    "revision": "d5826dad8b307e666ba44b636c671086",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1431017c668246829d22959b24a9cde8.js"
+  "/portfolio/precache-manifest.73ccd81d86278b3f8ca959642652e14d.js"
 );
 
 workbox.clientsClaim();
