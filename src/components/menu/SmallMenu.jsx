b1f680ba31c301dad38d2c583d00cc93b60475fb
sirishasamudrala5 <sirisha.samudrala5@gmail.com>
2023-04-06 10:29:50 +0530

    insta added in small menu

--- src/components/menu/SmallMenu.jsx
+++ src/components/menu/SmallMenu.jsx
@@ -18,6 +18,7 @@ const SmallMenu = (props) => {
     <footer id="footer-small">
         <nav>
             <ul>
+                <li><a href="https://www.instagram.com/code_g00d/" className="icon brands fa-instagram" target="_blank"><span className="label">Instagram</span></a></li>
                 <li><a href="https://www.linkedin.com/in/samudralasirisha/?originalSubdomain=in" className="icon brands fa-linkedin"><span className="label">LinkedIn</span></a></li>
                 <li><a href="https://github.com/sirishasamudrala5" className="icon brands fa-github"><span className="label">Github</span></a></li>
                 <li><a href="mailto:sirisha.samudrala15@gmail.com" className="icon solid fa-envelope"><span className="label">Email</span></a></li>
