20bebe8298c82f195db6b885c4713614825a7c47
sirisha pv <sirishapv@sirishas-MacBook-Pro.local>
2025-10-23 23:22:01 +0530

    updated resume and experience

--- src/components/common/CollapsableCard.jsx
+++ src/components/common/CollapsableCard.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react"
 import Collapsible from 'react-collapsible'
 
 const CollapsableCard = (props) => {
-    return <Collapsible trigger={[props.data.title, <span className="icon solid fa-caret-down"></span>]} triggerTagName={"h2"} open={props.data.title == "TATA 1MG" ?  true : false} easing="ease-in-out" transitionTime={600}>
-        <h2 className="title">{props.data.title}</h2>
+    return <Collapsible trigger={[props.data.title, <span className="icon solid fa-caret-down"></span>]} triggerTagName={"h2"} open={props.data.title == "Modern Spaaces" ?  true : false} easing="ease-in-out" transitionTime={600}>
+        
           <p className="role">{props.data.role}</p>
         <p className="subtitle">[{props.data.journey}]</p>
         <br />
